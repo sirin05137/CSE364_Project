caead9433090ff9e337bf8b6705b4264e3509ef5
Yujin Lee <yujinlee@unist.ac.kr>
2021-03-22 16:59:34 +0900

    Closes #2;Updated dockerfile, run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 git clone https://7431831df8332ebd730fab69f7dbdc329659ed45@github.com/sirin05137/CSE364_Project.git
 cd CSE364_Project
 mvn install
-java -cp target/cse364-project-1.0-SNAPSHOT-jar-withdependencies.jar xxx.yyy.YourClass Adventure educator
+#java -cp target/cse364-project-1.0-SNAPSHOT-jar-withdependencies.jar xxx.yyy.YourClass Adventure educator
