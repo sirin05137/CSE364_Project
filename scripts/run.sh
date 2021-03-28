34d228629bc17884bd456ba13f05143347b4a6be
Sanghun Lee <sanghun17@unist.ac.kr>
2021-03-28 21:48:36 +0900

    executable checked in sanghun's local

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
 git clone https://7431831df8332ebd730fab69f7dbdc329659ed45@github.com/sirin05137/CSE364_Project.git
 #git clone https://github.com/sirin05137/CSE364_Project.git
 cd CSE364_Project
+git checkout f468aabb7eaaf44bd3cde841e4ca793194e217f3
 mvn install
 java -cp target/cse364-project-1.0-SNAPSHOT.jar group11.project Drama scientist
--- src/main/java/group11/project.java
+++ src/main/java/group11/project.java
@@ -8,24 +8,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-/*class fournode{
-    static String A, B, C;
-    static int D=0;
-    static void setABC(String args){
-        A=args;
-    }
-    static void setB(String args){
-        B=args;
-    }
-    static void setC(String args){
-        C=args;
-    }
-    static void counter(){
-
-    }
-    //static String get()
-}*/
-
 public class project {
     static HashMap<String,String> InputMap(String args, int a) throws IOException{
         HashMap<String,String> givendata = new HashMap<String,String>();
@@ -44,25 +26,29 @@ public class project {
     }
     public static void main(String[] args) throws IOException{
 
-    	
+        if (args.length != 2){
+            System.out.println("\nINPUT_ERROR: 2 arguments needed (inputStr1 inputStr2)\n");
+            System.exit(0);
+        }
+        //System.out.println(args[0]);
+        //System.out.println(args[1]);
+
         //get occupation from users.dat, and make a hashmap which key means user id and value means occupation.
         HashMap<String,String> Occupation =  InputMap("data/users.dat", 3);
+        //System.out.println(Occupation);
 
         //get genre from movie.dat, and make a hashmap which key means movie id and value means genre
         HashMap<String,String> Genres = InputMap("data/movies.dat", 2);
         //System.out.println(Genres);
 
-
         //rating data read
-
         //double[][][] rating = new double[21][18][2]; 영화 장르가 2~3개인 경우가 았어서 이 방법은 불가능
-
-        //위에서 만든 해쉬맵을 참조하여 중첩맵을 생성, 중첩맵은 {장르 : {직업 : [평점, counter]}}를 나타넴
-        //중첩 map안에 Arraylist가 한번더 들어가 있는 형태
-        //{장르 : {직업 : [평점, counter]}}에서 counter는 평점 average를 구하기 위한 초석. 앞선 중첩된 맵의 평점은 그저 ""같은 직업의 사람이 같은 장르의 영화에 대한 평점을 다 더한것"". 평점/counter = 평균 평점
+        //위에서 만든 해쉬맵을 참조하여 중첩맵을 생성, 중첩맵은 {장르 : {직업 : [평점, counter]}}를 나타냄
+        //중첩 map안에 Arraylist가 한번 더 들어가 있는 형태
+        //{장르 : {직업 : [평점, counter]}}에서 counter는 평점 average를 구하기 위한 초석. 앞선 중첩된 맵의 평점은 그저 같은 직업의 사람이 같은 장르의 영화에 대한 평점을 다 더한것. 평점/counter = 평균 평점
 
         HashMap<String, HashMap<String, ArrayList<Integer>>> Rating = new HashMap<String, HashMap<String, ArrayList<Integer>>>();
-        BufferedReader getrating = new BufferedReader(new FileReader("./data/ratings.dat")); // need to change the location.
+        BufferedReader getrating = new BufferedReader(new FileReader("data/ratings.dat"));
 
         while (true) {
             HashMap<String, ArrayList<Integer>> innermap = new HashMap<String, ArrayList<Integer>>();
@@ -72,46 +58,39 @@ public class project {
                 break;
             }
             String[] word = line.split("::" );
-            //Rating data format = UserID::MovieID::Rating::Timestamp
-            //Genres data format = MovieID::Title::Genres
-            //Genres.get(word[1])=Genres.get(movie id)=genres
-            //Rating.get(Genres.get(word[1]))=Inner hashmap
-            //from below, make hashmap "Rating"
-            if(Rating.containsKey(Genres.get(word[1]))){//Rating이라는 해쉬맵이, rating 데이터로부터 읽은 장르를 이미 key로써 가지고 있을 경우
-            	//Occupation.get(word[0])=Occupation.get(UserID)=occupation
-            	//Rating.get(Genres.get(word[1])).get(Occupation.get(word[0]))=Inner hashmap.get(occupation)=data of inner hash maep = [더해진 평점, counter]
-                if(Rating.get(Genres.get(word[1])).containsKey(Occupation.get(word[0]))){//Rating이라는 해쉬맵의 데이터인 inner hash맵이, rating 데이터로부터 읽은 직업을 이미 key로써 가지고 있는 경우
-                    int a = Rating.get(Genres.get(word[1])).get(Occupation.get(word[0])).get(0) + Integer.parseInt(word[2]); // rating_sum = rating_sum + readed rating
+
+            if(Rating.containsKey(Genres.get(word[1]))){
+                if(Rating.get(Genres.get(word[1])).containsKey(Occupation.get(word[0]))){
+                    int a = Rating.get(Genres.get(word[1])).get(Occupation.get(word[0])).get(0) + Integer.parseInt(word[2]);
                     Rating.get(Genres.get(word[1])).get(Occupation.get(word[0])).set(0,a);
-                    int b = Rating.get(Genres.get(word[1])).get(Occupation.get(word[0])).get(1); //counter = counter +1
+                    int b = Rating.get(Genres.get(word[1])).get(Occupation.get(word[0])).get(1);
                     Rating.get(Genres.get(word[1])).get(Occupation.get(word[0])).set(1,b+1);
                 }
-                else {//Rating이라는 해쉬맵의 데이터인 Inner hash맵이, rating데이터로부터 읽은 직업을 key로 가지고 있지 않은 경우,
-                    Rating.get(Genres.get(word[1])).put(Occupation.get(word[0]), innerlist); //Ineer hash map에 해당 키와 데이터를 만들어주고
-                    innerlist.add(0, Integer.parseInt(word[2]));//rating_sum = readed rating (rating 초기화)
-                    innerlist.add(1, 1);//counter =1 (data 초기화)
+                else {
+                    Rating.get(Genres.get(word[1])).put(Occupation.get(word[0]), innerlist);
+                    innerlist.add(0, Integer.parseInt(word[2]));
+                    innerlist.add(1, 1);
                 }
             }
-            else{//rating이라는 해쉬맵에 , rating 데이터로부터 읽은 장르를 key로 가지고 있지 않은 경우,
-                Rating.put(Genres.get(word[1]), innermap);//Rating 해쉬맵에 해당 키(장르)와 데이터(inner hash map)를 만들어주고
-                innermap.put(Occupation.get(word[0]), innerlist); // inner hash map 의 키(직업)과 데이터(inner array)초기화
-                innerlist.add(0,Integer.parseInt(word[2])); //rating_sum = readed rating (rating 초기화)
-                innerlist.add(1, 1);//counter =1 (data 초기화)
+            else{
+                Rating.put(Genres.get(word[1]), innermap);
+                innermap.put(Occupation.get(word[0]), innerlist);
+                innerlist.add(0,Integer.parseInt(word[2]));
+                innerlist.add(1, 1);
             }
-
         }
         //System.out.println(Rating);
+        //숫자 좀 줄여서 제대로 만들어 지는지 테스트
 
-
-       /*String[] dboccupation= {"academic","educator","artist","clerical","admin","collegestudent",
-                "gradstudent","customerservice","doctor","healthcare","executive","managerial","farmer","homemaker",
-                "K-12student","lawyer","programmer","retired","sales","marketing","scientist","self-employed",
-                "technician","engineer","tradesman","craftsman","unemployed","writer","other"};
-                */
-       String[] dbgenre= {"Action", "Adventure", "Animation", "Children's","Comedy","Crime","Documentary","Drama","Fantasy","Film-Noir",
-    		   "Horror","Musical","Mystery","Romance","Sci-Fi","Thriller","War","Western"};
-       
-       //////////////////////////////////////
+        /*String[] dboccupation= {"academic","educator","artist","clerical","admin","college student",
+                "grad student","customer service","doctor","health care","executive","managerial","farmer","homemaker",
+                "K-12 student","lawyer","programmer","retired","sales","marketing","scientist","self-employed",
+                "technician","engineer","tradesman","craftsman","unemployed","writer","other"};*/
+String[] dbgenre= {"action", "adventure", "animation", "children's","comedy","crime","documentary","drama","fantasy","film-Noir",
+    		   "horror","musical","mystery","romance","sci-Fi","thriller","war","western"};
+        //입력에 따른 예외처리 조건문 필요
+        //Scanner inputreader = new Scanner(System.in);
+         //////////////////////////////////////
        // check genre input validity start //
        //////////////////////////////////////
        Scanner inputreader = new Scanner(System.in);
@@ -177,7 +156,28 @@ public class project {
        /////////////////////////////////////
        // check genre input validity end ///
        ////////////////////////////////////
-       
+
+        // 2. 장르의 조합이 없을경우 오류문구 출력 + 질문 재출력 -> 완료
+        // 3. 대소문자 구분없이 비교 -> 완료
+        // 4. 아무것도 입력 안했을때 -> 완료
+        // 5. delimiter랑 장르 사이에 빈칸있을때 -> 완료
+        // 6. split bug
+
+        // 대형버그 : adventure는 a를 포함해서 a만 입력해도 인식해버리는 상황 발생 -> 완료
+
+        String occupationinput = args[1].toLowerCase().trim();
+        /*
+        do{
+            System.out.print("\nEnter the occupation : ");
+            occupationinput= inputreader.nextLine().toLowerCase().trim();
+            if(occupationinput=="") {
+                System.out.println("Invalid input!");
+            }
+        }while(occupationinput=="");
+         */
+        // 3. 띄어쓰기 없어도 인식하게 할것
+        // 4. 아무것도 입력 안 했을때 -> 완료
+
         String OccupationNumber = null;
         switch (occupationinput){
             case "academic":
@@ -192,6 +192,8 @@ public class project {
                 OccupationNumber = "3";
                 break;
             case "college":
+            case "collegestudent":
+            case "grad":
             case "gradstudent":
                 OccupationNumber = "4";
                 break;
@@ -254,17 +256,21 @@ public class project {
         }
         //System.out.println(OccupationNumber);
 
+        //ERROR : 해당하는 직업군 이름이 없을 경우
+        if (OccupationNumber == "0"){
+            System.out.println("\nINPUT_ERROR: Entered occupation doesn't exist\n");
+            System.exit(0);
+        }
+
         double fullrating = 0;
         double fullcount = 0;
-        //입력 장르와 입력 직업을 고려해야함. 입력 직업은  if (Iter.getValue().containsKey(OccupationNumber)) 로 옳게 고려 되었음.
-        //입력 장르는 
         ArrayList<String> uncombinelists = new ArrayList<String>();
         for(Map.Entry<String,HashMap<String,ArrayList<Integer>>> Iter : Rating.entrySet()) {
             String[] uncombined = Iter.getKey().split("\\|");
             for (int k = 0; k < uncombined.length; k++) {
                 uncombinelists.add(uncombined[k].trim());
             }
-            if (uncombinelists.containsAll(inputlist)) { 
+            if (uncombinelists.containsAll(inputlist)) {
                 if (Iter.getValue().containsKey(OccupationNumber)) {
                     fullrating += (double) Iter.getValue().get(OccupationNumber).get(0);
                     fullcount += (double) Iter.getValue().get(OccupationNumber).get(1);
