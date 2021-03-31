53f5ef6f266d3f926ec62b6473f9cb856945bfb4
Yujin Lee <yujinlee@unist.ac.kr>
2021-04-01 01:03:59 +0900

    JUnit test & resources updated

--- src/test/java/group11/projectTest.java
+++ src/test/java/group11/projectTest.java
@@ -32,8 +32,14 @@ public class projectTest extends project {
         System.setErr(new PrintStream(errContent));
     }
 
-    /*
-    @Test
+    // --- 1genre 1occupation 모든 조합 결과값 나오나 확인하는 테스트 ---
+    // 테스트 결과 pass 기준 : (double)결과값 > 0
+    // 총 378 test cases (18 genres * 21 occupation)
+    // 테스트 전 에 꼭 project.java 마지막 부분 아래 print 문구를 :
+    // System.out.printf("\nThe rating of %s rated by %s : %.2f", genreinput, occupationinput, CalculatedInput);
+    // 이로 수정해주세요 :
+    // System.out.printf("\n%.2f", CalculatedInput);
+    @Test거
     @FileParameters(value = "src/test/resources/test1genre.csv", mapper = CsvWithHeaderMapper.class)
      public void test1genre(String genre, String occupation, String expected) throws IOException {
         String[] args = new String[2];
@@ -47,20 +53,10 @@ public class projectTest extends project {
         double result = Double.parseDouble(String.valueOf(outContent));
         assertTrue(result > 0);
     }
-    */
 
-    @Test
-    @FileParameters(value = "src/test/resources/test1genre_other.csv", mapper = CsvWithHeaderMapper.class)
-    public void test1genre_other(String genre, String occupation, String expected) throws IOException {
-        String[] args = new String[2];
-        args[0] = genre.toLowerCase().trim();
-        args[1] = occupation.toLowerCase().trim();
-
-        project.main(args);
-
-        assertEquals(expected, outContent.toString());
-    }
-
+    // --- multiple genres 결과값 맞나 확인하는 테스트 ---
+    // 테스트 결과 pass 기준 : (double)결과값 == expected(엑셀손계산)
+    // 총 23 test cases
     @Test
     @FileParameters(value = "src/test/resources/testMultiple.csv", mapper = CsvWithHeaderMapper.class)
     public void testMultiplegenre(String genre, String occupation, String expected) throws IOException {
