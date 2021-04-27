4c2b572268cdda9073159e0544c76851cf0b55aa
sirin05137 <nonename0416@naver.com>
2021-04-27 21:38:52 +0900

    Make calculation of total average rating

--- src/main/java/group11/milestone2.java
+++ src/main/java/group11/milestone2.java
@@ -3,16 +3,18 @@ package group11;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-class Noode{
+class Movie_data_node{
     String movieID;
     String title;
     String genre;
     String link;
-    double total_rating=0;
-    double counter=0;
+    int total_rating=0;
+    int counter=0;
 
     public void setMovieID(String movieID) {
         this.movieID = movieID;
@@ -20,13 +22,11 @@ class Noode{
     public void setTitle(String title) {
         this.title = title;
     }
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
+    public void setGenre(String genre) { this.genre = genre; }
     public void setLink(String link) {
         this.link = link;
     }
-    public void setTotal_rating(double total_rating) {
+    public void setTotal_rating(int total_rating) {
         this.total_rating += total_rating;
     }
     public void setCounter() {
@@ -44,10 +44,10 @@ class Noode{
     public String getLink() {
         return link;
     }
-    public double getTotal_rating() {
+    public int getTotal_rating() {
         return total_rating;
     }
-    public double getCounter() {
+    public int getCounter() {
         return counter;
     }
     public void print_node(){
@@ -57,14 +57,11 @@ class Noode{
 }
 
 public class milestone2 {
-    public static void main(String[] args) throws IOException {
 
-        //long start = System.currentTimeMillis();
-
-        ArrayList<Noode> movie_rating_matrix = new ArrayList<>();
+    static void make_table(ArrayList<Movie_data_node> movie_rating_matrix) throws IOException{
         BufferedReader get_movie_data = new BufferedReader(new FileReader("data/movies.dat"));
         while(true){
-            Noode inner_node = new Noode();
+            Movie_data_node inner_node = new Movie_data_node();
             String line = get_movie_data.readLine();
             if (line == null)
                 break;
@@ -98,13 +95,44 @@ public class milestone2 {
             String[] word = line.split("::");
             for (int i = 0; i < movie_rating_matrix.size(); i++) {
                 if (movie_rating_matrix.get(i).getMovieID().equals(word[1])) {
-                    movie_rating_matrix.get(i).setTotal_rating(Double.parseDouble(word[2]));
+                    movie_rating_matrix.get(i).setTotal_rating(Integer.parseInt(word[2]));
                     movie_rating_matrix.get(i).setCounter();
                     break;
                 }
             }
         }
         get_rating_data.close();
+
+    }
+    static double total_average_rating(ArrayList<Movie_data_node> movie_rating_matrix){
+        BigDecimal sum_of_average_rating = new BigDecimal("0.0");
+        int count = 0;
+        for(int i=0;i< movie_rating_matrix.size();i++){
+            if(movie_rating_matrix.get(i).getCounter()!=0){
+                BigDecimal a = new BigDecimal(String.valueOf(movie_rating_matrix.get(i).getTotal_rating()));
+                BigDecimal b = new BigDecimal(String.valueOf(movie_rating_matrix.get(i).getCounter()));
+                sum_of_average_rating=sum_of_average_rating.add(a.divide(b,3,RoundingMode.HALF_UP));
+                count++;
+            }
+        }
+        //System.out.println(count);
+        BigDecimal c = new BigDecimal(String.valueOf(count));
+        //System.out.println(sum_of_average_rating.doubleValue());
+        //System.out.println(c.doubleValue());
+
+        BigDecimal result = sum_of_average_rating.divide(c,2,RoundingMode.HALF_UP);
+        return result.doubleValue();
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        //long start = System.currentTimeMillis();
+
+        ArrayList<Movie_data_node> movie_rating_matrix = new ArrayList<>();
+        make_table(movie_rating_matrix);
+        double C =total_average_rating(movie_rating_matrix);
+
+
 
 
 
@@ -127,9 +155,9 @@ public class milestone2 {
 
 
         //long end = System.currentTimeMillis();
-        //System.out.println(end-start);
-        /*for (int i = 0; i < movie_data.size(); i++){
-            movie_data.get(i).print_node();
+        //System.out.println(end-start  );
+        /*for (int i = 0; i < movie_rating_matrix.size(); i++){
+            movie_rating_matrix.get(i).print_node();
         }*/
     }
 }
