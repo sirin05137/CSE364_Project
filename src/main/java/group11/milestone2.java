0f36a9b4d4e98c89b03570b9e08522dafd57e212
sirin05137 <nonename0416@naver.com>
2021-04-28 17:42:59 +0900

    Make simple recommendation system

--- src/main/java/group11/milestone2.java
+++ src/main/java/group11/milestone2.java
@@ -5,16 +5,16 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 class Movie_data_node{
     String movieID;
     String title;
     String genre;
-    String link;
+    //String link;
     int total_rating=0;
     int counter=0;
+    //double W=0;
 
     public void setMovieID(String movieID) {
         this.movieID = movieID;
@@ -23,15 +23,30 @@ class Movie_data_node{
         this.title = title;
     }
     public void setGenre(String genre) { this.genre = genre; }
-    public void setLink(String link) {
+    /*public void setLink(String link) {
         this.link = link;
-    }
+    }*/
     public void setTotal_rating(int total_rating) {
         this.total_rating += total_rating;
     }
     public void setCounter() {
         this.counter++;
     }
+    /*public void setW(double CC, int mm){
+        if(counter==0){
+            this.W=CC;
+        }
+        else {
+            BigDecimal tr = new BigDecimal(String.valueOf(total_rating));
+            BigDecimal v = new BigDecimal(String.valueOf(counter));
+            BigDecimal R = tr.divide(v, 2, RoundingMode.HALF_UP);
+            BigDecimal C = new BigDecimal(String.valueOf(CC));
+            BigDecimal m = new BigDecimal(String.valueOf(mm));
+            BigDecimal upper = v.multiply(R).add(m.multiply(C));
+            BigDecimal lower = v.add(m);
+            this.W = upper.divide(lower, 2, RoundingMode.HALF_UP).doubleValue();
+        }
+    }*/
     public String getMovieID() {
         return movieID;
     }
@@ -41,19 +56,72 @@ class Movie_data_node{
     public String getGenre() {
         return genre;
     }
-    public String getLink() {
+   /*public String getLink() {
         return link;
-    }
+    }*/
     public int getTotal_rating() {
         return total_rating;
     }
     public int getCounter() {
         return counter;
     }
+    //public double getW() { return W; }
     public void print_node(){
-        System.out.print("{movieID : "+this.movieID+", Title : "+this.title+", Genre : "+this.genre+", link : "
-                +this.link+", total_rating : "+this.total_rating+", counter : "+this.counter+"}");
+        /*System.out.print("{movieID : "+this.movieID+", Title : "+this.title+", Genre : "+this.genre+", link : "
+                +this.link+", total_rating : "+this.total_rating+", counter : "+this.counter+"}");*/
+        System.out.print("{movieID : "+this.movieID+", Title : "+this.title+", Genre : "+this.genre+", total_rating : "
+                           +this.total_rating+", counter : "+this.counter+"}");
     }
+}
+class Classified_by_vote extends Movie_data_node implements Comparable{
+    double W=0;
+    String link;
+
+    public int compareTo(Object o){
+        return compareTo((Classified_by_vote) o);
+    }
+    public int compareTo(Classified_by_vote another){
+        double thisVal = this.getW();
+        double anotherVal = another.getW();
+        return (thisVal<anotherVal ? 1 : (thisVal == anotherVal ? 0 : -1));
+    }
+
+    public Classified_by_vote(Movie_data_node movie_rating_data){
+        this.movieID = movie_rating_data.getMovieID();
+        this.title = movie_rating_data.getTitle();
+        this.genre = movie_rating_data.getGenre();
+        this.total_rating = movie_rating_data.getTotal_rating();
+        this.counter = movie_rating_data.getCounter();
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+    public void setW(double CC, int mm){
+        if(counter==0){
+            this.W=CC;
+        }
+        else {
+            BigDecimal tr = new BigDecimal(String.valueOf(total_rating));
+            BigDecimal v = new BigDecimal(String.valueOf(counter));
+            BigDecimal R = tr.divide(v, 3, RoundingMode.HALF_UP);
+            BigDecimal C = new BigDecimal(String.valueOf(CC));
+            BigDecimal m = new BigDecimal(String.valueOf(mm));
+            BigDecimal upper = v.multiply(R).add(m.multiply(C));
+            BigDecimal lower = v.add(m);
+            this.W = upper.divide(lower, 3, RoundingMode.HALF_UP).doubleValue();
+        }
+    }
+    public String getLink() {
+        return link;
+    }
+    public double getW() {
+         return W;
+     }
+     public void print_node(){
+        System.out.println("{movieID : "+this.movieID+", Title : "+this.title+", Genre : "+this.genre+", link : "
+                +this.link+", total_rating : "+this.total_rating+", counter : "+this.counter+", W : "+this.W+"}");
+     }
 }
 
 public class milestone2 {
@@ -73,7 +141,7 @@ public class milestone2 {
             movie_rating_matrix.add(inner_node);
         }
         get_movie_data.close();
-        BufferedReader get_link_data = new BufferedReader(new FileReader("data/links.dat"));
+        /*BufferedReader get_link_data = new BufferedReader(new FileReader("data/links.dat"));
         while(true){
             String line = get_link_data.readLine();
             if (line == null)
@@ -86,7 +154,7 @@ public class milestone2 {
                 }
             }
         }
-        get_link_data.close();
+        get_link_data.close();*/
         BufferedReader get_rating_data = new BufferedReader(new FileReader("data/ratings.dat"));
         while(true) {
             String line = get_rating_data.readLine();
@@ -123,6 +191,57 @@ public class milestone2 {
         BigDecimal result = sum_of_average_rating.divide(c,2,RoundingMode.HALF_UP);
         return result.doubleValue();
     }
+    static int Percentile(ArrayList<Movie_data_node> movie_rating_matrix, double p){
+        ArrayList<Integer> vote_counting_list = new ArrayList<Integer>();
+        for(int i=0;i<movie_rating_matrix.size();i++) {
+            if (movie_rating_matrix.get(i).getCounter() != 0) {
+                vote_counting_list.add(movie_rating_matrix.get(i).getCounter());
+            }
+        }
+        Collections.sort(vote_counting_list);
+        BigDecimal One = new BigDecimal("1.0");
+        BigDecimal ratio = new BigDecimal(String.valueOf(p));
+        BigDecimal num = new BigDecimal(String.valueOf(vote_counting_list.size()));
+        BigDecimal np = ratio.multiply(num);
+        if(np.remainder(One).doubleValue()==0.0){
+            //System.out.println("test");
+            int a = np.intValue();
+            return (vote_counting_list.get(a-1)+vote_counting_list.get(a))/2;
+        }
+        else{
+            //System.out.println("elsecase");
+            //System.out.println(vote_counting_list);
+            int b=np.intValue();
+            return vote_counting_list.get(b-1);
+        }
+    }
+    static void make_classified_table(ArrayList<Classified_by_vote> classified_table, ArrayList<Movie_data_node> movie_rating_matrix, double C, int m) throws IOException{
+        for(int i=0;i<movie_rating_matrix.size();i++){
+            if(movie_rating_matrix.get(i).getCounter()>=m){
+                Classified_by_vote inner_data = new Classified_by_vote(movie_rating_matrix.get(i));
+                classified_table.add(inner_data);
+            }
+        }
+        BufferedReader get_link_data = new BufferedReader(new FileReader("data/links.dat"));
+        while(true){
+            String line = get_link_data.readLine();
+            if (line == null)
+                break;
+            String[] word = line.split("::");
+            for(int i=0;i< classified_table.size();i++){
+                if(classified_table.get(i).getMovieID().equals(word[0])){
+                    classified_table.get(i).setLink("http://www.imdb.com/title/tt"+word[1]);
+                    break;
+                }
+            }
+        }
+        get_link_data.close();
+        for(int i=0;i<classified_table.size();i++){
+            classified_table.get(i).setW(C,m);
+        }
+        Collections.sort(classified_table);
+
+    }
 
     public static void main(String[] args) throws IOException {
 
@@ -131,6 +250,18 @@ public class milestone2 {
         ArrayList<Movie_data_node> movie_rating_matrix = new ArrayList<>();
         make_table(movie_rating_matrix);
         double C =total_average_rating(movie_rating_matrix);
+        int m = Percentile(movie_rating_matrix, 0.95);
+        //System.out.println(m);
+
+        ArrayList<Classified_by_vote> classified_table = new ArrayList<>();
+        make_classified_table(classified_table, movie_rating_matrix, C, m);
+        for(int i=0;i<10;i++){
+            System.out.println(classified_table.get(i).getTitle()+" ("+classified_table.get(i).getLink()+")");
+        }
+
+
+
+
 
 
 
@@ -158,6 +289,9 @@ public class milestone2 {
         //System.out.println(end-start  );
         /*for (int i = 0; i < movie_rating_matrix.size(); i++){
             movie_rating_matrix.get(i).print_node();
+        }*/
+        /*for(int i=0;i<classified_table.size();i++){
+            classified_table.get(i).print_node();
         }*/
     }
 }
