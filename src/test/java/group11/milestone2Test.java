3d285e847293b3a32c2040f0eecf1863ef2fd343
Yujin Lee <yujinlee@unist.ac.kr>
2021-05-01 14:58:34 +0900

    ADD JunitTest (84% coverage)

--- src/test/java/group11/milestone2Test.java
+++ src/test/java/group11/milestone2Test.java
@@ -12,21 +12,84 @@ import org.junit.runner.RunWith;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class milestone2Test extends milestone2 {
 
-    private String emptyValue = null;
+    //private final String emptyValue = null;
 
     @Test
-    public void test() throws IOException {
-        String[] args = new String[3];
-        args[0] = emptyValue;
-        args[1] = emptyValue;
-        args[2] = emptyValue;
+    public void valid_genre_test() {
+        String[] args = new String[]{"action", "adventure", "animation"};
+        assertTrue(milestone2.check_genre_validity(args));
+    }
 
-        milestone2.main(args);
-        //assertEquals(expected, outContent.toString());
+    @Test
+    public void invalid_genre_test() {
+        String[] args = new String[]{"action", "advvvvventure", "animacccccion"};
+        assertFalse(milestone2.check_genre_validity(args));
+    }
+
+    //------------------------------------------------------------------
+    /*
+    @Test
+    public void average_rating_test(){
+
+        HashMap<String, ArrayList<Integer>> h_map = ;
+        double val;
+        assertNotNull(milestone2.percentile(h_map, val));
+    }
+    */
+    @Test
+    public void occu_input_test() throws IOException {
+        HashMap<String, ArrayList<String>> user_data = milestone2.make_user_data();
+
+        String[] occu_list = new String[]{
+                "academic", "artist", "admin", "grad", "customerservice",
+                "doctor", "executive", "farmer", "homemaker", "k-12student",
+                "lawyer", "programmer", "retired", "marketing", "scientist",
+                "self-employed", "engineer", "tradesman", "unemployed",
+                "writer", "ARBITRARYINPUT" /*-> Last one falls to occunum 0*/
+        };
+
+        for (String occu_input : occu_list) {
+            assertAll( () -> milestone2.make_user_list_occu(user_data, occu_input));
+        }
+    }
+
+    //------------------------------------------------------------------
+
+    @Test
+    public void empty_3args_test() {
+        String[] args = new String[3];
+        args[0] = "";
+        args[1] = "";
+        args[2] = "";
+
+        assertAll( () -> milestone2.main(args));
+    }
+
+    @Test
+    public void valid_3args_test() {
+        String[] args = new String[3];
+        args[0] = "F";
+        args[1] = "56";
+        args[2] = "retired";
+
+        assertAll( () -> milestone2.main(args));
+    }
+
+    @Test
+    public void empty_4args_test() {
+        String[] args = new String[4];
+        args[0] = "";
+        args[1] = "";
+        args[2] = "";
+        args[3] = "Action";
+
+        assertAll( () -> milestone2.main(args));
     }
 }
