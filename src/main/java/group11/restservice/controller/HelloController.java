3a770dd691eff99c39d6c5412bff844c3358c5f8
Yujin Lee <yujinlee@unist.ac.kr>
2021-06-18 05:37:02 +0900

    Table jquery

--- src/main/java/group11/restservice/controller/HelloController.java
+++ src/main/java/group11/restservice/controller/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -19,9 +20,23 @@ public class HelloController {
         this.recoRepository = recoRepository;
     }
 
-    @RequestMapping("/index.html")
+    @RequestMapping("/")
     public List<RecoData> helloWorld() {
-        return this.recoRepository.findAll();
+        RecoData rd1 = new RecoData("1","Toy Story","Animation","http://www.imdb.com/title/tt0114709","https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@..jpg");
+        RecoData rd2 = new RecoData("2","Jumanji","Adventure","http://www.imdb.com/title/tt0113497","https://m.media-amazon.com/images/M/MV5BZTk2ZmUwYmEtNTcwZS00YmMyLWFkYjMtNTRmZDA3YWExMjc2XkEyXkFqcGdeQXVyMTQxNzMzNDI@..jpg");
+        RecoData rd3 = new RecoData("2","Jumanji","Adventure","http://www.imdb.com/title/tt0113497","https://m.media-amazon.com/images/M/MV5BZTk2ZmUwYmEtNTcwZS00YmMyLWFkYjMtNTRmZDA3YWExMjc2XkEyXkFqcGdeQXVyMTQxNzMzNDI@..jpg");
+        RecoData rd4 = new RecoData("2","Jumanji","Adventure","http://www.imdb.com/title/tt0113497","https://m.media-amazon.com/images/M/MV5BZTk2ZmUwYmEtNTcwZS00YmMyLWFkYjMtNTRmZDA3YWExMjc2XkEyXkFqcGdeQXVyMTQxNzMzNDI@..jpg");
+        RecoData rd5 = new RecoData("2","Jumanji","Adventure","http://www.imdb.com/title/tt0113497","https://m.media-amazon.com/images/M/MV5BZTk2ZmUwYmEtNTcwZS00YmMyLWFkYjMtNTRmZDA3YWExMjc2XkEyXkFqcGdeQXVyMTQxNzMzNDI@..jpg");
+        RecoData rd6 = new RecoData("2","Jumanji","Adventure","http://www.imdb.com/title/tt0113497","https://m.media-amazon.com/images/M/MV5BZTk2ZmUwYmEtNTcwZS00YmMyLWFkYjMtNTRmZDA3YWExMjc2XkEyXkFqcGdeQXVyMTQxNzMzNDI@..jpg");
+        RecoData rd7 = new RecoData("2","Jumanji","Adventure","http://www.imdb.com/title/tt0113497","https://m.media-amazon.com/images/M/MV5BZTk2ZmUwYmEtNTcwZS00YmMyLWFkYjMtNTRmZDA3YWExMjc2XkEyXkFqcGdeQXVyMTQxNzMzNDI@..jpg");
+        RecoData rd8 = new RecoData("2","Jumanji","Adventure","http://www.imdb.com/title/tt0113497","https://m.media-amazon.com/images/M/MV5BZTk2ZmUwYmEtNTcwZS00YmMyLWFkYjMtNTRmZDA3YWExMjc2XkEyXkFqcGdeQXVyMTQxNzMzNDI@..jpg");
+        RecoData rd9 = new RecoData("2","Jumanji","Adventure","http://www.imdb.com/title/tt0113497","https://m.media-amazon.com/images/M/MV5BZTk2ZmUwYmEtNTcwZS00YmMyLWFkYjMtNTRmZDA3YWExMjc2XkEyXkFqcGdeQXVyMTQxNzMzNDI@..jpg");
+        RecoData rd10 = new RecoData("2","Jumanji","Adventure","http://www.imdb.com/title/tt0113497","https://m.media-amazon.com/images/M/MV5BZTk2ZmUwYmEtNTcwZS00YmMyLWFkYjMtNTRmZDA3YWExMjc2XkEyXkFqcGdeQXVyMTQxNzMzNDI@..jpg");
+        RecoData rd11 = new RecoData("2","Jumanji","Adventure","http://www.imdb.com/title/tt0113497","https://m.media-amazon.com/images/M/MV5BZTk2ZmUwYmEtNTcwZS00YmMyLWFkYjMtNTRmZDA3YWExMjc2XkEyXkFqcGdeQXVyMTQxNzMzNDI@..jpg");
+        RecoData rd12 = new RecoData("2","Jumanji","Adventure","http://www.imdb.com/title/tt0113497","https://m.media-amazon.com/images/M/MV5BZTk2ZmUwYmEtNTcwZS00YmMyLWFkYjMtNTRmZDA3YWExMjc2XkEyXkFqcGdeQXVyMTQxNzMzNDI@..jpg");
+        RecoData rd13 = new RecoData("2","Jumanji","Adventure","http://www.imdb.com/title/tt0113497","https://m.media-amazon.com/images/M/MV5BZTk2ZmUwYmEtNTcwZS00YmMyLWFkYjMtNTRmZDA3YWExMjc2XkEyXkFqcGdeQXVyMTQxNzMzNDI@..jpg");
+
+        return Arrays.asList(rd1,rd2,rd3,rd4,rd5,rd6,rd7,rd8,rd9,rd10,rd11,rd12,rd13);
     }
 
 }
