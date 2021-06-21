f2b2f759a3da733e5f25cf2a511a18549954f3cf
sirin05137 <nonename0416@naver.com>
2021-06-21 22:19:24 +0900

    WAR file deploy ver

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,6 @@ git clone https://7431831df8332ebd730fab69f7dbdc329659ed45@github.com/sirin05137
 #git clone https://github.com/sirin05137/CSE364_Project.git
 cd CSE364_Project
 mvn install
+mongod --fork --logpath /var/log/mongodb.log
 ./mvnw spring-boot:run
 
